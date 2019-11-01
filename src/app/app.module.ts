65ce3b23f251864a83d3b297f835f27b7b7a2be4
jkmalan <jkmalan@tootallgaming.com>
2019-11-01 14:29:45 -0400

    [ADD] Add Angular 8 framework
    This branch is intended as an experimental site design

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    declarations: [
+
+    ],
+    entryComponents: [
+
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+
+    ],
+    bootstrap: [
+
+    ]
+})
+export class AppModule {}
