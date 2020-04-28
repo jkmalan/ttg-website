a61e3df66eb592ab2049eb2509368d078604ce51
jkmalan <jkmalan@tootallgaming.com>
2020-04-27 20:00:28 -0400

    [MOD] Refactor project for Twig templating

---
+++ private/initialize.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+include '../private/initialize.php';
+
+echo $twig->render('maintenance.html.twig', ['title' => 'Maintenance']);
+
+
